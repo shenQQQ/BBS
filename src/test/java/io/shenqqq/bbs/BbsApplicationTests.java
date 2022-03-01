fc07e11bc57a51393fa19562ad295338140236e4
ShenQi <1017706039@qq.com>
2022-03-01 14:51:12 +0800

    first commit

---
+++ src/test/java/io/shenqqq/bbs/BbsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.shenqqq.bbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
